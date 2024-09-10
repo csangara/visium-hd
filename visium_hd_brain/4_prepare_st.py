fa963d685e42165350c3a8a2a67b7d047b8b41d9
Chananchida Sang-aram VSCODE <sai.chananchida@gmail.com>
2024-09-10 17:10:34 +0200

    uddate prepare_st

--- visium_hd_brain/4_prepare_st.py
+++ visium_hd_brain/4_prepare_st.py
@@ -4,4 +4,9 @@ file_path = os.environ['VSC_DATA_VO_USER'] + '/spatial_datasets/Visium_HD_Mouse_
 
 # Read anndata from 10x output
 adata = sc.read_10x_h5(file_path + 'filtered_feature_bc_matrix.h5')
+
+# Change var_names to gene ids
+adata.var["gene_names"] = adata.var_names
+adata.var_names = adata.var["gene_ids"]
+
 adata.write(os.environ['VSC_DATA_VO_USER'] + '/rds/Visium_HD_Mouse_Brain_008um.h5ad')
