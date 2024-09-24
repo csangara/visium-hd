16dad3779f0e088cf79a9971ff9cd7982bfd2160
Chananchida HPC <chananchida.sangaram@ugent.be>
2024-09-24 15:51:32 +0200

    change directory

--- visium_hd_brain/4_prepare_st.py
+++ visium_hd_brain/4_prepare_st.py
@@ -1,6 +1,6 @@
 import os
 import scanpy as sc
-file_path = os.environ['VSC_DATA_VO_USER'] + '/spatial_datasets/Visium_HD_Mouse_Brain/square_008um/'
+file_path = os.environ['VSC_DATA_VO_USER'] + '/spatial_datasets/Visium_HD_MouseBrain/square_008um/'
 
 # Read anndata from 10x output
 adata = sc.read_10x_h5(file_path + 'filtered_feature_bc_matrix.h5')
@@ -9,4 +9,4 @@ adata = sc.read_10x_h5(file_path + 'filtered_feature_bc_matrix.h5')
 adata.var["gene_names"] = adata.var_names
 adata.var_names = adata.var["gene_ids"]
 
-adata.write(os.environ['VSC_DATA_VO_USER'] + '/rds/Visium_HD_Mouse_Brain_008um.h5ad')
+adata.write(os.environ['VSC_DATA_VO_USER'] + '/spatial_datasets/Visium_HD_MouseBrain_008um.h5ad')
