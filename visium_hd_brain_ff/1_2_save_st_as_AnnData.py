e2443234df4105de110059fcf070c4b3663e1a7d
Chananchida HPC <chananchida.sangaram@ugent.be>
2025-01-22 13:49:29 +0100

    fresh frozen scripts

---
+++ visium_hd_brain_ff/1_2_save_st_as_AnnData.py
@@ -0,0 +1,12 @@
+import os
+import scanpy as sc
+file_path = os.environ['VSC_DATA_VO_USER'] + '/spatial_datasets/Visium_HD_MouseBrain_FF/binned_outputs/square_008um/'
+
+# Read anndata from 10x output
+adata = sc.read_10x_h5(file_path + 'filtered_feature_bc_matrix.h5')
+
+# Change var_names to gene ids
+adata.var["gene_names"] = adata.var_names
+adata.var_names = adata.var["gene_ids"]
+
+adata.write(os.environ['VSC_DATA_VO_USER'] + '/spatial_datasets/Visium_HD_MouseBrain_FF/Visium_HD_MouseBrain_FF_008um.h5ad')
