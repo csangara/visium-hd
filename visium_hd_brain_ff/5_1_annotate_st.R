ce6a0bcd66560005deee4c8fb7b07a163faf1260
Chananchida Sang-aram VSCODE <sai.chananchida@gmail.com>
2025-11-20 17:47:09 +0100

    brain region annotation scripts

--- visium_hd_brain_ff/5_1_annotate_st.R
+++ visium_hd_brain_ff/5_1_annotate_st.R
@@ -9,7 +9,7 @@ library(jsonlite)
 #### TRANSFERRING REGION ANNOTATIONS TO VISIUM DATA ####
 # Load transformed atlas map (from QuickNII or VisuAlign)
 
-coronal_visualign_map <- load.image("data/Visium_HD_MouseBrain/QUINT/tissue_8um_lowres_image_nl.png")
+coronal_visualign_map <- load.image("data/Visium_HD_MouseBrain_FF/QUINT/tissue_lowres_image_nl.png")
 
 # Read parcellations file
 parcellations_color <- read.csv("data/ABA_metadata/Allen-CCF-2020/20230630/views/parcellation_to_parcellation_term_membership_color.csv")
@@ -17,7 +17,7 @@ parcellations_acronym <- read.csv("data/ABA_metadata/Allen-CCF-2020/20230630/vie
 parcellations <- inner_join(parcellations_color, parcellations_acronym,
                                  by="parcellation_index")
 # Load Visium image
-coronal_visium_img <- load.image("data/Visium_HD_MouseBrain/binned_outputs/square_008um/spatial/tissue_lowres_image.png")
+coronal_visium_img <- load.image("data/Visium_HD_MouseBrain_FF/binned_outputs/square_008um/spatial/tissue_lowres_image.png")
 
 # Resize the atlas map as the Visium image size
 dim(coronal_visium_img)[1:2]
@@ -28,10 +28,10 @@ plot(coronal_visualign_map)          # They seem similar
 plot(coronal_visualign_map_resize)
 
 # Load scale factors
-scale_factors <- fromJSON("data/Visium_HD_MouseBrain/binned_outputs/square_008um/spatial/scalefactors_json.json")
+scale_factors <- fromJSON("data/Visium_HD_MouseBrain_FF/binned_outputs/square_008um/spatial/scalefactors_json.json")
 
 # Load Visium coordinates
-coronal_visium_coords <- arrow::read_parquet("data/Visium_HD_MouseBrain/binned_outputs/square_008um/spatial/tissue_positions.parquet") %>% 
+coronal_visium_coords <- arrow::read_parquet("data/Visium_HD_MouseBrain_FF/binned_outputs/square_008um/spatial/tissue_positions.parquet") %>% 
   mutate(pxl_row_in_lowres = pxl_row_in_fullres * scale_factors$tissue_lowres_scalef,
          pxl_col_in_lowres = pxl_col_in_fullres * scale_factors$tissue_lowres_scalef) %>% 
   filter(in_tissue == 1)
@@ -46,6 +46,10 @@ ggplot(df,aes(x,y)) + geom_raster(aes(fill=rgb.val)) +
   geom_point(inherit.aes=FALSE, data=coronal_visium_coords,
              aes(y=pxl_row_in_lowres, x=pxl_col_in_lowres), size=0.05, color="red", alpha=0.1) +
   scale_fill_identity() + scale_y_reverse() + coord_fixed(ratio=1)
+
+# Load the "color to region name" dictionary file from https://www.nitrc.org/forum/message.php?msg_id=32830
+#rainbow_map <- jsonlite::fromJSON("data/Visium_HD_MouseBrain/QUINT/Rainbow 2017.json")
+# rainbow_map <- rainbow_map %>% mutate(rgb.val=rgb(red, green, blue, maxColorValue = 255))
 
 
 # Number of regions in the dictionary vs in the picture
@@ -85,13 +89,15 @@ coords_to_region <- left_join(x = coords_to_color,
 division_colors_df <- parcellations %>% distinct(division, division_color)
 division_colors <- setNames(division_colors_df$division_color, division_colors_df$division)
 
-# Let's check the regions
+# Let's check the regions interactively
+library(plotly)
 p <- ggplot(coords_to_region,aes(y=pxl_row_in_lowres, x=pxl_col_in_lowres, fill=division)) +
   geom_bin2d(bins=200) + scale_y_reverse() + coord_fixed(ratio=1) +
   scale_fill_manual(values = division_colors) +
   theme_classic() +
   theme()
 p
+ggplotly(p)
 
 # Save the metadata
-saveRDS(coords_to_region, "data/Visium_HD_MouseBrain/tissue_positions_with_annotations_008um.rds")
+saveRDS(coords_to_region, "data/Visium_HD_MouseBrain_FF/tissue_positions_with_annotations_008um.rds")
